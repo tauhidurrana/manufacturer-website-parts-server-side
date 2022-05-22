37d3d85656244a396051fb595dadd2b5d89256d2
tauhidurrana <tauhidurrahman.rana@gmail.com>
2022-05-22 21:58:45 +0600

    fetch data from mongodb to client side successfully

--- index.js
+++ index.js
@@ -12,7 +12,29 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sp5uy.mongodb.net/?retryWrites=true&w=majority`;
-// console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        await client.connect();
+        const productCollection = client.db('computer-parts-manufacturer').collection('products');
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello from computer Manufacturer!')
